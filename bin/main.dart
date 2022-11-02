8a80eeb29bcad045f7fb3ec9717e46a0a0744a15
Adrian Kohls <adriankohls95@gmail.com>
2022-11-02 08:00:58 -0300

    first commit

---
+++ bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:rabbit_mq/main.dart';
+
+void main(List<String> arguments) {
+  run('adriankohls');
+}
---
+++ lib/constants.dart
@@ -0,0 +1,5 @@
+const host = 'jackal.rmq.cloudamqp.com';
+const username = 'lnixlvfr:lnixlvfr';
+const password = 'eRX8pkRaL99ai9FmgIlKLcjP_hjyb_Vw';
+
+enum MqttTopics { event, test, rest }
